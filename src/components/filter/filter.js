a71ac99f3a95c1e4e2dc6fe453b4014fb2bf4b67
Tobias Ramos <tobiasramossilva@gmail.com>
2023-07-03 13:51:00 -0300

    Criando o componente que filtrar os países pela região

---
+++ src/components/filter/filter.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./filter.css";
+
+const Filter = ({ handleFilter }) => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleFilterChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+    handleFilter(selectedValue);
+  };
+  return (
+    <div>
+      <select value={selectedOption} onChange={handleFilterChange}>
+        <option>Filtar por região</option>
+        <option value="Africa">África</option>
+        <option value="Americas">América</option>
+        <option value="Asia">Ásia</option>
+        <option value="Europe">Europa</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
--- src/page-home/home.js
+++ src/page-home/home.js
@@ -2,22 +2,36 @@ import "./home.css";
 import data from "../data/data.json";
 import { useState } from "react";
 import Search from "../components/search/search";
+import Filter from "../components/filter/filter";
 
 const Home = () => {
   const [countries, setCountries] = useState(data);
+  const [filteredCountries, setFilteredCountries] = useState(data);
 
   const handleSearch = (searchCountries) => {
-    const results = data.filter((country) =>
+    const results = countries.filter((country) =>
       country.name.toLowerCase().includes(searchCountries.toLowerCase())
     );
-    setCountries(results);
+    setFilteredCountries(results);
+  };
+
+  const handleFilter = (selectedRegion) => {
+    if (selectedRegion === "") {
+      setFilteredCountries(countries);
+    } else {
+      const results = countries.filter(
+        (country) => country.region === selectedRegion
+      );
+      setFilteredCountries(results);
+    }
   };
 
   return (
     <>
       <Search handleSearch={handleSearch} />
+      <Filter handleFilter={handleFilter} />
       <div className="country-container">
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <div className="country">
             <img src={country.flags.png} alt={"Logo do " + country.name} />
             <p>{country.name}</p>
