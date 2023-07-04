ea0714f858b6c265500d93520eb9caea71dbc8e7
Tobias Ramos <tobiasramossilva@gmail.com>
2023-07-04 20:39:04 -0300

    Terminando as estilizações no componente header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 import "./header.css";
+import { MdOutlineDarkMode } from "react-icons/md";
 
 const Header = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    const currentMode = localStorage.getItem('darkMode');
-    if (currentMode === 'true') {
+    const currentMode = localStorage.getItem("darkMode");
+    if (currentMode === "true") {
       setIsDarkMode(true);
-      document.body.classList.add('dark');
+      document.body.classList.add("dark");
     }
   }, []);
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-    document.body.classList.toggle('dark');
-    localStorage.setItem('darkMode', !isDarkMode);
+    document.body.classList.toggle("dark");
+    localStorage.setItem("darkMode", !isDarkMode);
   };
 
   return (
     <header className="header-container">
-      <h1>Qual país?</h1>
-      <div className={`dark-mode-toggle ${isDarkMode ? 'dark' : 'light'}`} onClick={toggleDarkMode}>
-      <span>{isDarkMode ? 'Modo Claro' : 'Modo Escuro'}</span>
-    </div>
+      <h2>Qual país?</h2>
+      <div
+        className={`dark-mode-toggle ${isDarkMode ? "dark" : "light"}`}
+        onClick={toggleDarkMode}
+      >
+        <MdOutlineDarkMode className='icon' />
+        <span>{isDarkMode ? "Modo Claro" : "Modo Escuro"}</span>
+      </div>
     </header>
   );
 };
