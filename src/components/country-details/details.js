7699afbe8f09dd5c1b662acaa4956fcc3462d07c
Tobias Ramos <tobiasramossilva@gmail.com>
2023-07-05 14:36:55 -0300

    Tornando o componente details mais semântico

--- src/components/country-details/details.js
+++ src/components/country-details/details.js
@@ -1,7 +1,8 @@
-import "./details.css";
-import data from "../../data/data.json";
 import { Link } from "react-router-dom";
 import numeral from "numeral";
+
+import "./details.css";
+import data from "../../data/data.json";
 
 const CountryDetails = ({ match }) => {
   const countryName = match.params.name;
@@ -15,21 +16,23 @@ const CountryDetails = ({ match }) => {
   return (
     <div className="details-container">
       <Link to="/">Voltar</Link>
-      <div className="datails">
+      <article className="datails">
         <img src={country.flags.png} alt={"Logo do " + country.name} />
-        <div>
+        <div className="description">
           <h1>{country.name}</h1>
-          <p>Nome nativo: {country.nativeName}</p>
-          <p>População: {numeral(country.population).format('0,0')}</p>
-          <p>Região: {country.region}</p>
-          <p>Sub região: {country.subregion}</p>
-          <p>Capital: {country.capital}</p>
-          <p>Domínio de nível superior: {country.topLevelDomain}</p>
-          <p>Moedas: {country.currencies[0].name}</p>
-          <p>Linguagens: {country.languages[0].name}</p>
-          <p>Países fronteiriçõs: {country.borders.join(" ")}</p>
+          <div className="descriptions">
+            <p>Nome nativo: {country.nativeName}</p>
+            <p>População: {numeral(country.population).format("0,0")}</p>
+            <p>Região: {country.region}</p>
+            <p>Sub região: {country.subregion}</p>
+            <p>Capital: {country.capital}</p>
+            <p>Domínio de nível superior: {country.topLevelDomain}</p>
+            <p>Moedas: {country.currencies[0].name}</p>
+            <p>Linguagens: {country.languages[0].name}</p>
+            <p>Países fronteiriçõs: {country.borders.join(" ")}</p>
+          </div>
         </div>
-      </div>
+      </article>
     </div>
   );
 };
